d88e73c4fa49bda7cdd04dee104e5c15fda72a03
Ridah <190897538+Yathribu@users.noreply.github.com>
2026-04-02 16:31:51 +0200

    feat: ajout repository + accès bdd

--- src/main/java/com/example/prototypeai/ai/controller/AskAiController.java
+++ src/main/java/com/example/prototypeai/ai/controller/AskAiController.java
@@ -3,6 +3,8 @@ package com.example.prototypeai.ai.controller;
 import com.example.prototypeai.ai.dto.AskAiDto;
 import com.example.prototypeai.ai.service.AskAiService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,9 +18,8 @@ public class AskAiController {
     }
 
     @PostMapping()
-    public AskAiDto.PostOutput postAskAiFromUser(@Valid @RequestBody AskAiDto.PostInput request) {
-
-        return aiServices.sendRequest(request.getUserRequest(), request.getAiProvider());
+    public ResponseEntity<AskAiDto.PostOutput> postAskAiFromUser(@Valid @RequestBody AskAiDto.PostInput request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(aiServices.sendRequest(request.getUserRequest(), request.getAiProvider()));
     }
 
 }
